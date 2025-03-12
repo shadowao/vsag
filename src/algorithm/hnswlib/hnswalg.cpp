08e2877408aa50bf7f1a7677198a23a2427b0205
jinjiabao.jjb <jinjiabao.jjb@antgroup.com>
2025-03-12 15:55:29 +0800

    add tsan pipeline
    Signed-off-by: jinjiabao.jjb <jinjiabao.jjb@antgroup.com>

--- src/algorithm/hnswlib/hnswalg.cpp
+++ src/algorithm/hnswlib/hnswalg.cpp
@@ -308,6 +308,7 @@ std::priority_queue<std::pair<float, LabelType>>
 HierarchicalNSW::bruteForce(const void* data_point,
                             int64_t k,
                             const vsag::FilterPtr is_id_allowed) const {
+    std::shared_lock resize_lock(resize_mutex_);
     std::priority_queue<std::pair<float, LabelType>> results;
     for (uint32_t i = 0; i < cur_element_count_; i++) {
         if (is_id_allowed && not is_id_allowed->CheckValid(getExternalLabel(i))) {
@@ -1171,6 +1172,7 @@ HierarchicalNSW::updateVector(LabelType label, const void* data_point) {
         // reset data
         std::shared_ptr<float[]> normalize_data;
         normalizeVector(data_point, normalize_data);
+        std::unique_lock resize_lock(resize_mutex_);
         memcpy(getDataByInternalId(internal_id), data_point, data_size_);
     }
 }
@@ -1190,6 +1192,7 @@ HierarchicalNSW::updateLabel(LabelType old_label, LabelType new_label) {
         // reset label
         label_lookup_.erase(iter_old);
         label_lookup_[new_label] = internal_id;
+        std::unique_lock resize_lock(resize_mutex_);
         setExternalLabel(internal_id, new_label);
     }
 }
