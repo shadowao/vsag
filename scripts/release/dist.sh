c13160815112b2d427b2588f20f82bc99f9dd28b
Xiangyu Wang <wxy407827@antgroup.com>
2025-04-25 00:08:49 +0800

    [skip ci]update binary distribution script (#639)
    Signed-off-by: Xiangyu Wang <wxy407827@antgroup.com>

--- scripts/release/dist.sh
+++ scripts/release/dist.sh
@@ -12,7 +12,7 @@ docker run -u $CURRENT_UID:$CURRENT_GID --rm -v $(pwd):/work vsag-builder \
        bash -c "\
        export COMPILE_JOBS=48 && \
        export CMAKE_INSTALL_PREFIX=/tmp/vsag && \
-       make clean && make dist-old-abi && make install && \
+       make clean-release && make dist-old-abi && make install && \
        mkdir -p ./dist && \
        cp -r /tmp/vsag ./dist/ && \
        cd ./dist && \
@@ -28,7 +28,7 @@ docker run -u $CURRENT_UID:$CURRENT_GID --rm -v $(pwd):/work vsag-builder \
        bash -c "\
        export COMPILE_JOBS=48 && \
        export CMAKE_INSTALL_PREFIX=/tmp/vsag && \
-       make clean && make dist-cxx11-abi && make install && \
+       make clean-release && make dist-cxx11-abi && make install && \
        mkdir -p ./dist && \
        cp -r /tmp/vsag ./dist/ && \
        cd ./dist && \
@@ -39,18 +39,19 @@ version=$(git describe --tags --always --dirty --match "v*")
 dist_name=vsag-$version-cxx11-abi.tar.gz
 mv dist/vsag.tar.gz dist/$dist_name
 
+# FIXME(wxyu): libcxx deps on clang17, but it cannot install via yum directly
 # libcxx version
-docker run -u $CURRENT_UID:$CURRENT_GID --rm -v $(pwd):/work vsag-builder \
-       bash -c "\
-       export COMPILE_JOBS=48 && \
-       export CMAKE_INSTALL_PREFIX=/tmp/vsag && \
-       make clean && make dist-libcxx && make install && \
-       mkdir -p ./dist && \
-       cp -r /tmp/vsag ./dist/ && \
-       cd ./dist && \
-       rm -r ./vsag/lib && mv ./vsag/lib64 ./vsag/lib && \
-       tar czvf vsag.tar.gz ./vsag && rm -r ./vsag
-"
-version=$(git describe --tags --always --dirty --match "v*")
-dist_name=vsag-$version-libcxx.tar.gz
-mv dist/vsag.tar.gz dist/$dist_name
+# docker run -u $CURRENT_UID:$CURRENT_GID --rm -v $(pwd):/work vsag-builder \
+#        bash -c "\
+#        export COMPILE_JOBS=48 && \
+#        export CMAKE_INSTALL_PREFIX=/tmp/vsag && \
+#        make clean-release && make dist-libcxx && make install && \
+#        mkdir -p ./dist && \
+#        cp -r /tmp/vsag ./dist/ && \
+#        cd ./dist && \
+#        rm -r ./vsag/lib && mv ./vsag/lib64 ./vsag/lib && \
+#        tar czvf vsag.tar.gz ./vsag && rm -r ./vsag
+# "
+# version=$(git describe --tags --always --dirty --match "v*")
+# dist_name=vsag-$version-libcxx.tar.gz
+# mv dist/vsag.tar.gz dist/$dist_name
