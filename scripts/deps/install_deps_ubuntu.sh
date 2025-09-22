71957ece12fb653128113d254fc695b1ff138ae2
LHT129 <tianlan.lht@antgroup.com>
2025-09-22 14:10:12 +0800

    fix(neon): asan problem on load data (#1174)
    Signed-off-by: LHT129 <tianlan.lht@antgroup.com>

--- scripts/deps/install_deps_ubuntu.sh
+++ scripts/deps/install_deps_ubuntu.sh
@@ -5,7 +5,7 @@ if [[ "$arch" == "x86_64" ]]; then
     apt update && apt install -y gfortran python3-dev libomp-15-dev gcc make cmake g++ lcov libaio-dev intel-mkl libcurl4-openssl-dev
 elif [[ "$arch" == "aarch64" ]]; then
     echo "Executing apt install for aarch64"
-    apt update && apt install -y gfortran python3-dev libomp-15-dev gcc make cmake g++ lcov libaio-dev libopenblas-dev libcurl4-openssl-dev
+    apt update && apt install -y gfortran python3-dev libomp-15-dev gcc make cmake g++ lcov libaio-dev libcurl4-openssl-dev
 else
     echo "Unknown architecture: $arch"
 fi
