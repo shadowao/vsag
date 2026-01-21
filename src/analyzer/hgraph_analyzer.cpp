5e95c0d4bea3b4ee8b26206bda4622adb27b4dda
baoyuan <129934985+misaka0714@users.noreply.github.com>
2026-01-21 11:01:14 +0800

    fix(analyzer): add nullptr check and copy query vectors in HGraphAnalyzer (#1528)
    Signed-off-by: baoyuan <129934985+misaka0714@users.noreply.github.com>

--- src/analyzer/hgraph_analyzer.cpp
+++ src/analyzer/hgraph_analyzer.cpp
@@ -249,6 +249,14 @@ HGraphAnalyzer::SetQuery(const DatasetPtr& query) {
     query_sample_ids_.resize(query_sample_size_);
     query_sample_datas_.resize(static_cast<std::vector<float>::size_type>(query_sample_size_) *
                                static_cast<std::vector<float>::size_type>(dim_));
+    if (const float* query_vectors = query->GetFloat32Vectors()) {
+        std::memcpy(query_sample_datas_.data(),
+                    query_vectors,
+                    static_cast<unsigned long>(query_sample_size_ * dim_) * sizeof(float));
+    } else {
+        logger::error("Query dataset is empty or not float32");
+        return false;
+    }
     std::iota(query_sample_ids_.begin(), query_sample_ids_.end(), 0);
     return true;
 }
